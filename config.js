0d994369c6b89d1746b791a69ac083f7f543c981
Awat | Studio13 <awat.s4lih@gmail.com>
2026-03-14 12:41:24 +0100

    update frontend supabase publishable key

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 window.SUPABASE_URL = "https://iuvjmctrzgztelrsuquc.supabase.co";
-window.SUPABASE_ANON_KEY = "sb_publishable_uC3_z0Mcxbjk68IWCh2ItQ_lxSR6_fJ";
+window.SUPABASE_ANON_KEY = "sb_publishable_nTRieOP-2JjOoYf91lnY_Q_bl15qFe-";
